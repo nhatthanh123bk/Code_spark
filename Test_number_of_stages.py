e7c2d2c17ef55ed13a0a5f7f66aac5972cd4851a
nhatthanh123bk <nhatthanh123bk@gmail.com>
2019-09-17 13:59:59 +0700

    first commit

--- Test_number_of_stages.py
+++ Test_number_of_stages.py
@@ -2,33 +2,30 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField
 from pyspark.sql.types import DoubleType, IntegerType, StringType
 
-def main():
-   	spark = SparkSession\
-            .builder\
-            .appName("Test_number_of_stages")\
-            .getOrCreate()
-    
-    schema_ratings = StructType([
-				    StructField("userId",IntegerType()),\
-				    StructField("movieId", IntegerType()),\
-				    StructField("rating", DoubleType()),\
-				    StructField("timestamp",StringType())    
-					])   
+spark = SparkSession\
+    .builder\
+    .appName("Test_number_of_stages")\
+    .getOrCreate()
 
-    schema_movies = StructType([
-    				StructField("ID_movie",IntegerType()),\
-    				StructField("Name_movie",StringType()),\
-    				StructField("Stype_movie",StringType())
-    				])
+schema_ratings = StructType([
+			    StructField("userId",IntegerType()),\
+			    StructField("movieId", IntegerType()),\
+			    StructField("rating", DoubleType()),\
+			    StructField("timestamp",StringType())    
+				])   
+
+schema_movies = StructType([
+				StructField("ID_movie",IntegerType()),\
+				StructField("Name_movie",StringType()),\
+				StructField("Stype_movie",StringType())
+				])
 # stage load data
-    df_ratings = spark.read.format("csv").schema(schema_ratings).load("/nhatthanh/data/ml-20m/ratings.csv")
-    df_movies = spark.read.format("csv").schema(schema_movies).load("/nhatthanh/data/ml-20m/movies.csv")
+df_ratings = spark.read.format("csv").schema(schema_ratings).load("/nhatthanh/data/ml-20m/ratings.csv")
+df_movies = spark.read.format("csv").schema(schema_movies).load("/nhatthanh/data/ml-20m/movies.csv")
 # stage join
-    ra = df_ratings.alias('ra')
-    mo = df_movies.alias('mo')
-    inner_join = ra.join(mo, ra.movieId == mo.ID_movie)
+ra = df_ratings.alias('ra')
+mo = df_movies.alias('mo')
+inner_join = ra.join(mo, ra.movieId == mo.ID_movie)
 
-if __name__ == '__main__':
-	main()	
-    
+
 
