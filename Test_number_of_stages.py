1412b6c11462860fde941f5ea80f0525023e74d5
nhatthanh123bk <nhatthanh123bk@gmail.com>
2019-09-17 14:04:37 +0700

    first commit

--- Test_number_of_stages.py
+++ Test_number_of_stages.py
@@ -26,6 +26,6 @@ df_movies = spark.read.format("csv").schema(schema_movies).load("/nhatthanh/data
 ra = df_ratings.alias('ra')
 mo = df_movies.alias('mo')
 inner_join = ra.join(mo, ra.movieId == mo.ID_movie)
-
+inner_join.show()
 
 
